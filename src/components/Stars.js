ef38432c4ae7a024ed29ce30e9f00b30ce14361d
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-11 19:30:53 +0300

    refactoring:single page star rating

---
+++ src/components/Stars.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+const Stars = ({ stars, reviews }) => {
+  console.log(stars, reviews)
+
+
+
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
+    return (
+
+      <span key={index}>
+        {stars >= index + 1 ? (<BsStarFill />) : stars >= number ? (<BsStarHalf />) : (<BsStar />)}
+      </span >)
+  })
+  // console.log(tempStars)
+  return <Wrapper>
+    <div className="stars">
+      { /* star*/}
+      {tempStars}
+      {/* <span>
+        {stars >= 2 ? (<BsStarFill />) : stars >= 1.5 ? (<BsStarHalf />) : (<BsStar />)}
+      </span>
+      <span>
+        {stars >= 3 ? (<BsStarFill />) : stars >= 2.5 ? (<BsStarHalf />) : (<BsStar />)}
+      </span>
+      <span>
+        {stars >= 4 ? (<BsStarFill />) : stars >= 3.5 ? (<BsStarHalf />) : (<BsStar />)}
+      </span>
+      <span>
+        {stars === 5 ? (<BsStarFill />) : stars >= 4.5 ? (<BsStarHalf />) : (<BsStar />)}
+      </span> */}
+      { /* end of star*/}
+    </div>
+    <div className="reviews">({reviews} customer reviews)</div>
+  </Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    color: #ffb900;
+    font-size: 1rem;
+    margin-right: 0.25rem;
+  }
+  p {
+    margin-left: 0.5rem;
+    margin-bottom: 0;
+  }
+  margin-bottom: 0.5rem;
+`
+export default Stars
