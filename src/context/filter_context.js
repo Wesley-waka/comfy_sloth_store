e161950ca4b4cb6a95da0fc8695875edb500b726
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-11 20:54:49 +0300

    all products:sort feature(adding functionality)

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,8 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
+  sort: 'price-lowest'
 }
 
 const FilterContext = React.createContext()
@@ -27,8 +28,22 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+  const updateSort = (e) => {
+    // for demo
+    const { name, value } = e.target
+    console.log(name, value)
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
   return (
-    <FilterContext.Provider value={{ ...state }}>
+    <FilterContext.Provider value={{ ...state, setGridView, setListView, updateSort, }}>
       {children}
     </FilterContext.Provider>
   )
---
+++ src/reducers/filter_reducer.js
@@ -0,0 +1,32 @@
+import {
+  LOAD_PRODUCTS,
+  SET_LISTVIEW,
+  SET_GRIDVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from '../actions'
+
+const filter_reducer = (state, action) => {
+  if (action.type === LOAD_PRODUCTS) {
+    return {
+      ...state, all_products: [...action.payload], filtered_products: [...action.payload],
+
+    }
+  }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true }
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false }
+  }
+  if (action.type) {
+    return { ...state, sort: action.payload }
+  }
+  // return state
+  throw new Error(`No Matching "${action.type}" - action type`)
+}
+
+export default filter_reducer
