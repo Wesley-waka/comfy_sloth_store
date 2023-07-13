c5811ddb871b8f7fd77420412d3738c7cde46d67
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-13 16:24:31 +0300

    Auth0Provider:responsible for login && sign up

--- src/index.js
+++ src/index.js
@@ -2,18 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// dev-bgl7q1jcjwnen30i.us.auth0.com
+// BB6LuOvArlkD9hjzUdOcGJhhwKtlIkWv
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-bgl7q1jcjwnen30i.us.auth0.com"
+    clientId="BB6LuOvArlkD9hjzUdOcGJhhwKtlIkWv"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
