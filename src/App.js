7741d9445f425eb04e2c81856560bce8d1fbbcd2
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-13 15:07:18 +0300

    bug: fixed imports && exports in index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 import { Error, About, SingleProduct, Cart, Checkout, Products, Home, PrivateRoute } from './pages'
 
@@ -8,35 +8,33 @@ function App() {
     <Router>
       <Navbar />
       <Sidebar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
+      <Route exact path='/'>
+        <Home />
+      </Route>
 
-        <Route exact path='/about'>
-          <About />
-        </Route>
+      <Route exact path='/about'>
+        <About />
+      </Route>
 
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
+      <Route exact path='/cart'>
+        <Cart />
+      </Route>
 
-        <Route exact path='/products'>
-          <Products />
-        </Route>
+      <Route exact path='/products'>
+        <Products />
+      </Route>
 
-        <Route exact path='/products/:id' children={<SingleProduct />}>
-          <Products />
-        </Route>
-        <Route exact path='/checkout'>
-          <Checkout />
-        </Route>
+      <Route exact path='/products/:id' children={<SingleProduct />}>
+        <Products />
+      </Route>
+      <Route exact path='/checkout'>
+        <Checkout />
+      </Route>
 
-        <Route exact path='/error'>
-          <Error />
-        </Route>
+      <Route exact path='/error'>
+        <Error />
+      </Route>
 
-      </Switch>
       <Footer />
     </Router >
   )
